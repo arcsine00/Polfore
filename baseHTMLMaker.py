dca2dc765056569079bb5596d1c57d336c3ce8ed
arcsine00 <86476438+arcsine00@users.noreply.github.com>
2024-03-31 21:41:04 +0900

    Update baseHTMLMaker.py

--- baseHTMLMaker.py
+++ baseHTMLMaker.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
-import itertools
 import re
 
 class HTMLTag:
-    def __init__(self, name: str, content: str = '', **kwargs):
+    def __init__(self, name: str, content: str = '', CLASS: str = '', **kwargs):
         self.tagType = name
-        self.opener = f'<{name}'
+        self.opener = f'<{name}'+f' class={CLASS}'*(not not CLASS)
         for att, cont in kwargs.items():
             self.opener += f' {att}="{cont}"'
         self.opener += '>'
@@ -13,7 +12,7 @@ class HTMLTag:
         self.content = content
         self.text = f'{self.opener}{self.content}{self.exiter}'
         self.children = [self.content]
-    
+
     def adopted(self, children: list[HTMLTag]):
         for child in children:
             self.children.append(child.text)
@@ -21,13 +20,17 @@ class HTMLTag:
         self.text = f'{self.opener}{self.content}{self.exiter}'
         return self
 
+
 class Stylesheet(HTMLTag):
     def __init__(self, cont: str, issrc: bool = False, **kwargs):
-        super().__init__('link'*issrc+'style'*(not issrc), cont*(not issrc), href=cont*issrc, rel='stylesheet'*issrc, type='text/css', **kwargs)
+        super().__init__('link' * issrc + 'style' * (not issrc), cont * (not issrc), href=cont * issrc,
+                         rel='stylesheet' * issrc, type='text/css', **kwargs)
+
 
 class Script(HTMLTag):
     def __init__(self, cont: str, issrc: bool = False, **kwargs):
-        super().__init__('script', cont*(not issrc), src=cont*issrc, type='text/javascript', **kwargs)
+        super().__init__('script', cont * (not issrc), src=cont * issrc, type='text/javascript', **kwargs)
+
 
 def createGChart(chart: str, data: list, divid: str, title: str = '', width: int = 80, height: int = 500):
     s = f'''google.charts.load('current', {{'packages':['corechart']}});
@@ -42,10 +45,27 @@ def createGChart(chart: str, data: list, divid: str, title: str = '', width: int
     }}'''
     return [Script(s), HTMLTag('div', id=divid, style=f'width:{width}%; height:{height}px; border: 1px solid #ccc')]
 
-def groupGCharts(charts: list[list]):
-    return {'head': [Script('https://www.gstatic.com/charts/loader.js', True)]+[i[0] for i in charts], 'body': [i[1] for i in charts]}
 
-basis = {'head': [HTMLTag('meta', charset='utf-8'), HTMLTag('title', 'Polfore')], 'body': []}
+def groupGCharts(charts: list[list]):
+    return {'head': [Script('https://www.gstatic.com/charts/loader.js', True)] + [i[0] for i in charts],
+            'body': [i[1] for i in charts]}
+
+
+default_basis = {'head': [HTMLTag('meta', charset='utf-8'),
+                  HTMLTag('title', 'Polfore'),
+                  Stylesheet(
+                      'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+                      True,
+                      integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH',
+                      crossorigin='anonymous'),
+                  Script(
+                      'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+                      True,
+                      integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz',
+                      crossorigin='anonymous')
+                  ],
+         'body': []}
+
 
 class Document:
     def __init__(self, name: str, basis: dict, head: list[HTMLTag], body: list[HTMLTag]):
