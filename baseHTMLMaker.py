cca3570441db2c70dcafd97e818e43592bb60b69
arcsine00 <86476438+arcsine00@users.noreply.github.com>
2024-04-02 15:17:51 +0900

    Update baseHTMLMaker.py

--- baseHTMLMaker.py
+++ baseHTMLMaker.py
@@ -88,7 +88,8 @@ default_basis = {'head': [HTMLTag('meta', charset='utf-8'),
                       'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
                       True,
                       integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz',
-                      crossorigin='anonymous')
+                      crossorigin='anonymous'),
+                  Stylesheet('/static/assets/common.css', True)
                   ],
          'body': [
              Navbar('폴포어', {
